a9523cfd1cd84bf691f7f4d185327bc4892b8982
Mike Knerr <53237403+mkner@users.noreply.github.com>
2021-12-27 21:46:32 -0500

    Add files via upload

---
+++ WK 2/bargraph_display/bar_graph_running_display.ino
@@ -0,0 +1,29 @@
+/*
+  Test 10 segment LED bar graph display unit
+
+  -Mike Knerr
+  
+  Produces a running display on an LED bar graph
+
+*/
+
+#define LED_10 11
+#define LED_01 2
+#define LED_DELAY 200
+
+void setup() {
+  //configure pins to LED graph
+  for(int i=LED_01; i <= LED_10; i++)
+    pinMode(i,OUTPUT);
+}
+
+
+void loop() {
+  // keep running the LEDs sequentially until forever
+  for(int i=LED_01; i <= LED_10; i++)
+    {
+    digitalWrite(i,HIGH);
+    delay(LED_DELAY);
+    digitalWrite(i,LOW);
+    }
+}
